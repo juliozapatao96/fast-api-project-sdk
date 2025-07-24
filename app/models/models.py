67d1dc4f3f31a2be5b055f031b0e4e97cacfb1f4
juliozapatao96 <julio.cesar.zaor@gmail.com>
2025-07-23 19:42:07 -0500

    models of the remaining tables are created

--- app/models/models.py
+++ app/models/models.py
@@ -18,3 +18,25 @@ class Performance(Base):
     fantasy_points = Column(Float, nullable=False)
     player_id = Column(Integer, ForeignKey("player.player_id"), nullable=False)
     last_changed_date = Column(Date, nullable=False)
+
+
+class TeamPlayer(Base):
+    __tablename__="team_player"
+    team_id = Column(Integer, ForeignKey("team.team_id"), primary_key=True, index=True)
+    player_id = Column(Integer, ForeignKey("player.player_id"),primary_key=True, index=True)
+    last_changed_date = Column(Date, nullable=False)
+
+class Team(Base):
+    __tablename__="team"
+    team_id = Column(Integer, primary_key=True, index=True)
+    team_name = Column(String, nullable=False)
+    last_changed_date = Column(Date, nullable=False)
+    league_id = Column(Integer, ForeignKey("league.league_id"))
+
+
+class League(Base):
+    __tablename__="league"
+    league_id = Column(Integer, primary_key=True, index=True)
+    league_name = Column(String, nullable=False)
+    scoring_type = Column(String, nullable=False)
+    last_changed_date = Column(Date, nullable=False)
