6395905d2360577f5c2511970a2b01708cf4b2d7
juliozapatao96 <julio.cesar.zaor@gmail.com>
2025-07-24 13:17:50 -0500

    crear y probar get_players

--- app/crud/crud.py
+++ app/crud/crud.py
@@ -8,3 +8,15 @@ def get_player(db: Session, player_id: int):
     return db.query(models.Player).filter(
         models.Player.player_id == player_id).first()
     
+def get_players(db: Session, skip: int = 0, limit: int = 100,
+                min_last_changed_date: date = None,
+                last_name: str = None, first_name: str = None):
+    query = db.query(models.Player)
+
+    if min_last_changed_date:
+        query = query.filter(models.Player.last_changed_date >= min_last_changed_date)
+    if first_name:
+        query = query.filter(models.Player.first_name == first_name)
+    if last_name:
+        query = query.filter(models.Player.last_name == last_name)
+    return query.offset(skip).limit(limit).all()
--- fa_project.py
+++ fa_project.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from app.core.database import engine
-from app.crud.crud import get_player
+from app.crud.crud import get_player, get_players
 
 
 SessionLocal = sessionmaker(bind=engine)
@@ -9,6 +9,9 @@ try:
     session = SessionLocal()
     player = get_player(session, 1002)
     print(player.first_name)
+    # test get_players
+    players = get_players(session, last_name="Crosby")
+    print(players[0].last_name)
 except Exception as e:
     print(f"Error durante ejecución crud: {e}")
 finally:
